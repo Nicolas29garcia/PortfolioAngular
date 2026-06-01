cc3feb90a92b935ca6b6756e2886940c0257683d
Nicolas Henrique <20241ctb0100076@estudantes.ifpr.edu.br>
2026-06-01 20:00:50 +0000

    Aula15: Configura ambiente Angular com Material e primeira tela do Portfolio

--- portfolio-angular/src/app/app.ts
+++ portfolio-angular/src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+ import { MatListModule } from  '@angular/material/list' ;
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports : [ MatListModule ],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
